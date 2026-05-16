4af537169d8769812d5b89384418614655115e24
Iconejey <nicolas.gwy@gmail.com>
2026-05-16 17:09:00 +0200

    Add toggle  overview bar

--- configs/astal/app.js
+++ configs/astal/app.js
@@ -1,5 +1,5 @@
 import { App, Astal, Gtk, Gdk } from 'astal/gtk3';
-import { Variable } from 'astal';
+import { Variable, execAsync } from 'astal';
 import { Window, CenterBox, Box, Label } from 'astal/gtk3/widget';
 
 const time = Variable('').poll(1000, "date '+%H:%M:%S'");
@@ -8,6 +8,10 @@ function Bar(monitor) {
 	return new Window({
 		monitor,
 		className: 'Bar',
+		name: 'Overview',
+		namespace: 'overview_bar',
+		layer: Astal.Layer.OVERLAY,
+		margin: 0,
 		exclusivity: Astal.Exclusivity.EXCLUSIVE,
 		anchor: Astal.WindowAnchor.TOP | Astal.WindowAnchor.LEFT | Astal.WindowAnchor.RIGHT,
 		child: new CenterBox({
@@ -26,9 +30,42 @@ function Bar(monitor) {
 	});
 }
 
+let isMenu = false;
+let activeWindows = [];
+
 App.start({
 	css: 'style.css',
+	instanceName: 'astal',
+	requestHandler(request, res) {
+		if (request === 'toggle') {
+			isMenu = !isMenu;
+
+			// Toggle bar visibility
+			activeWindows.forEach(w => {
+				if (isMenu) {
+					w.show_all();
+					w.visible = true;
+				} else {
+					w.hide();
+				}
+			});
+
+			// Scale down workspace using hyprctl gaps
+			if (isMenu) {
+				execAsync('hyprctl keyword general:gaps_out 60').catch(print);
+				execAsync('hyprctl keyword general:gaps_in 15').catch(print);
+				execAsync('hyprctl keyword decoration:rounding 20').catch(print);
+			} else {
+				execAsync('hyprctl keyword general:gaps_out 20').catch(print);
+				execAsync('hyprctl keyword general:gaps_in 5').catch(print);
+				execAsync('hyprctl keyword decoration:rounding 10').catch(print); // Default rounding? Make sure to match default
+			}
+
+			return res('ok');
+		}
+		res('unknown');
+	},
 	main() {
-		App.get_monitors().map(Bar);
+		activeWindows = App.get_monitors().map(Bar);
 	}
 });
